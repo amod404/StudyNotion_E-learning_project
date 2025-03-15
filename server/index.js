730af41c4d898ce5822a2a8250763efc5e54469c
Amod <amodschool1234@gmail.com>
2025-03-15 22:03:49 +0530

    changed index

--- server/index.js
+++ server/index.js
@@ -1,55 +1,64 @@
-// app connect
+// Importing necessary modules and packages
 const express = require("express");
 const app = express();
-
-const courseRoutes = require('./routes/Course');
-const paymentRoutes = require('./routes/Payments');
-const profileRoutes = require('./routes/Profile');
-const userRoutes = require('./routes/User');
-
-const {connectDB} = require("./config/database")
-const { cloudinaryConnect } = require("./config/cloudinary")
+const userRoutes = require("./routes/user");
+const profileRoutes = require("./routes/profile");
+const courseRoutes = require("./routes/Course");
+const paymentRoutes = require("./routes/Payments");
+const contactUsRoute = require("./routes/Contact");
+const database = require("./config/database");
 const cookieParser = require("cookie-parser");
-const cors = require("cors")
-const fileUpload = require("express-fileupload")
+const cors = require("cors");
+const { cloudinaryConnect } = require("./config/cloudinary");
+const fileUpload = require("express-fileupload");
+const dotenv = require("dotenv");
 
-require('dotenv').config();
-const PORT = process.env.PORT || 5000;
+// Setting up port number
+const PORT = process.env.PORT || 4000;
 
-//db connect
-connectDB();
+// Loading environment variables from .env file
+dotenv.config();
 
+// Connecting to database
+database.connect();
+ 
+// Middlewares
 app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:"http://localhost:3000", //fontend ko entertain
-        credentials:true,
+        origin: "*",
+        credentials: true,
     })
-)
-app.use(fileUpload({useTempFiles: true, tempFileDir: '/tmp/'}));
-// app.use(express.urlencoded({extended:true, limit: '5mb'}));
-   
-//cloudinary connect
+);
+app.use(
+    fileUpload({
+        useTempFiles: true,
+        tempFileDir: "/tmp/",
+    })
+);
+
+// Connecting to cloudinary
 cloudinaryConnect();
 
-//routes
-app.use('/api/v1/auth', userRoutes);
-app.use('/api/v1/profile', profileRoutes);
-app.use('/api/v1/course', courseRoutes);
-app.use('/api/v1/payment', paymentRoutes);
+// Setting up routes
+app.use("/api/v1/auth", userRoutes);
+app.use("/api/v1/profile", profileRoutes);
+app.use("/api/v1/course", courseRoutes);
+app.use("/api/v1/payment", paymentRoutes);
+app.use("/api/v1/reach", contactUsRoute);
 
-//def route
-
-app.get("/", (req,res) => {
+// Testing the server
+app.get("/", (req, res) => {
     return res.json({
-        success:true,
-        message:"Your server is running ..... "
+        success: true,
+        message: "Your server is up and running ...",
     });
 });
 
-//activate the server
+// Listening to the server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+    console.log(`App is listening at ${PORT}`);
 });
 
+// End of code.
